14fb38cbf06c5354fda3432bbe45a27d424b4e69
Anushka Agrawal <101547118+Anushkaagrawal22@users.noreply.github.com>
2023-04-26 19:26:20 +0530

    Update code.py

--- code.py
+++ code.py
@@ -331,31 +331,3 @@ chose_action()
 def PrintGrid(positions):
     print('\n'.join(' '.join(str(x) for x in row) for row in positions))
     print()
-
-# def main():
-    
-    
-#     # print("************ Sample output of your program *******")
-
-
-#     # print('Player 2 (Q-learning)')
-#     # print('Action selected : 2')
-#     # print('Value of next state according to Q-learning : .7312')
-#     # # PrintGrid(game1)
-
-
-#     # print('Player 1 (MCTS with 25 playouts')
-#     # print('Action selected : 1')
-#     # print('Total playouts for next state: 5')
-#     # print('Value of next state according to MCTS : .1231')
-#     # # PrintGrid(game2)
-
-#     # print('Player 2 (Q-learning)')
-#     # print('Action selected : 2')
-#     # print('Value of next state : 1')
-#     # # PrintGrid(game3)
-    
-#     # print('Player 2 has WON. Total moves = 14.')
-    
-# if __name__=='__main__':
-#     main()
